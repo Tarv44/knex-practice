f345e7724cb87933b096785d48495ede8b0cb93f
Jonah Tarver <jonahdevine@gmail.com>
2021-01-06 02:37:51 -0800

    Added four basic queries for shopping_list

---
+++ src/drills.js
@@ -0,0 +1,65 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+    client: 'pg',
+    connection: process.env.DB_URL
+})
+
+console.log('knex and driver installed correctly');
+
+function searchByName(searchTerm) {
+    knexInstance
+        .select('id', 'name', 'price', 'category')
+        .from('shopping_list')
+        .where('name', 'ILIKE', `%${searchTerm}%`)
+        .then(result => {
+            console.log(result)
+        })
+}
+
+// searchByName('fish')
+
+function paginateList(pageNumber) {
+    const productsPerPage = 6
+    const offset = productsPerPage * (pageNumber -1)
+    knexInstance
+        .select('id', 'name', 'price', 'category')
+        .from('shopping_list')
+        .limit(productsPerPage)
+        .offset(offset)
+        .then(result => {
+            console.log(result)
+        })
+}
+
+// paginateList(2)
+
+function getItemsAfterDaysAgo(daysAgo) {
+    knexInstance
+        .select('id', 'name', 'price', 'category')
+        .from('shopping_list')
+        .where(
+            'date_added',
+            '>',
+            knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo)
+        )
+        .then(result => {
+            console.log(result)
+        })
+}
+
+// getItemsAfterDaysAgo(2)
+
+function getCostByCategory() {
+    knexInstance
+        .select('category')
+        .sum('price as total')
+        .from('shopping_list')
+        .groupBy('category')
+        .then(result => {
+            console.log(result)
+        })
+}
+
+getCostByCategory()
